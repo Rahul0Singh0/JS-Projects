efaaaa33c84fad072566b1e15e43841f7944f191
Rahul Singh <thesincererahul@gmail.com>
2025-03-01 17:20:07 +0530

    Added bounce to the ball

---
+++ Ping Pong/script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    let table = document.getElementById('ping-pong-table');
+    let ball = document.getElementById('ball'); 
+
+    // here the ballX and ballY will be helping us to set a starting point of ball w.r.t to table
+    let ballX = 10; // distance of the top of the ball w.r.t ping pong table
+    let ballY = 10; // distance of the left of the ball w.r.t ping pong table
+
+    let dx = 2; // displacement factor in x-direction, 2 -> you will displace by 2 px in positive x-direction, -2 -> you will displace by 2 px in negative x-direction
+    let dy = 2; // displacement factor in y-direction, 2 -> you will displace by 2 px in positive y-direction, -2 -> you will displace by 2 px in negative y-direction
+
+    ball.style.left = `${ballX}px`;
+    ball.style.top = `${ballY}px`;
+
+    setInterval(function exec() {
+    
+        ballX += dx;
+        ballY += dy;
+    
+        ball.style.left = `${ballX}px`;
+        ball.style.top = `${ballY}px`;
+    
+        // if(ballX > 700-20 || ballX <= 0) dx *= -1;
+        // if(ballY > 400-20 || ballY <= 0) dy *= -1;
+    
+        if(ballX > table.offsetWidth - ball.offsetWidth || ballX <= 0) dx *= -1; // change x-direction
+        if(ballY > table.offsetHeight - ball.offsetHeight || ballY <= 0) dy *= -1; // change y-direction
+
+    }, 1);
+
+});
